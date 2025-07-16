12f6960218fe5bed45ee05f5b335ed3b55f31281
Bruceliu1111 <mr.bruceliu@gmail.com>
2025-07-16 14:18:32 +0800

    add v-for

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
     <h1>Hello World from SFC(single file component)</h1>
     <ul>
-        <course-intro id="poop" name="python and oop" duration=35></course-intro>
+        <course-intro v-for="course in courses" :key="course.id" :id="course.id" :name="course.name"
+            :duration="course.duration" :current="course.current"></course-intro>
+        <!-- <course-intro id="poop" name="python and oop" duration=35></course-intro>
         <course-intro id="bdpy" name="python and big data" duration=42></course-intro>
-
+        <course-intro id="pykt" name="python and big data" duration="8"></course-intro> -->
     </ul>
 </template>
 
@@ -11,7 +13,11 @@
 export default {
     data() {
         return {
-            courses: [{ id: "poop", name: "python oop", duration: 35 }]
+            courses: [{ id: "poop", name: "python oop", duration: 35, current: true },
+            { id: "bdpy", name: "python and big data", duration:  42, current: false },
+            { id: "pykt", name: 'keras and tensorflow', duration: 44, current: true },
+            { id: "swift", name: 'iOS use Swift4.0', duration: 58, current: true }
+            ]
         }
     }
 }
--- src/components/CourseIntro.vue
+++ src/components/CourseIntro.vue
@@ -1,27 +1,39 @@
 <template>
     <li>
-    <h2>{{ id }}</h2>
+        <h2>{{ id }}-Current:{{ current }}-isCurrent:{{ isCurrent }}</h2>
         <button @click="toggleCourseDetail">show detail</button>
+        <button @click="toggleCurrent">toggle isCurrent status</button>
         <ul v-if="detailsVisible">
             <li>{{ name }}</li>
             <li>{{ duration }}</li>
         </ul>
-
-
     </li>
 </template>
 
 <script>
 export default {
-    props: ["id", "name", "duration"],
+    props: {
+        id: { type: String, required: true },
+        name: { type: String, required: true },
+        duration: {
+            type: Number, required: true, validator: function (value) {
+                return value > 7
+            }
+        },
+        current: { type: Boolean, required: false, default: false }
+    },
     data() {
         return {
             detailsVisible: true,
+            isCurrent: this.current
         }
     },
     methods: {
         toggleCourseDetail() {
             this.detailsVisible = !this.detailsVisible
+        },
+        toggleCurrent() {
+            this.isCurrent = !this.isCurrent;
         }
     }
 }
@@ -36,18 +48,18 @@ export default {
 #app li {
     box-shadow: 0 4px 8px rgba(0, 0, 128, 0.26);
     margin: 1rem auto;
-    border-radius: 10px;
+    border-radius: 8px;
     text-align: center;
     width: 50%;
     max-width: 40rem;
-    background-color: #f5ba83;
+    background-color: #b6fab6;
 }
 
 #app button {
     font: inherit;
     cursor: pointer;
     border: 1px solid #FF0077;
-    background-color: #C0FFEE;
+    background-color: #f3baf3;
     color: blue;
 
 }
