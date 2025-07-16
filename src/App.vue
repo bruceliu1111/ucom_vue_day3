f31319479a992c4c5e9393cacea7ad9cdeb3bf5e
Bruceliu1111 <mr.bruceliu@gmail.com>
2025-07-16 13:22:38 +0800

    add Pros

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <template>
-    <H1>Hello World from SFC(single file component)</H1>
+    <h1>Hello World from SFC(single file component)</h1>
+    <ul>
+        <course-intro id="poop" name="python and oop" duration=35></course-intro>
+        <course-intro id="bdpy" name="python and big data" duration=42></course-intro>
+
+    </ul>
 </template>
 
 <script>
